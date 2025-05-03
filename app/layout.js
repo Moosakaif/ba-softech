d6eb00f0281814f944b2d8073924aff42e0733f8
Abdul Haseeb Hamid <111380595+haseebx16@users.noreply.github.com>
2025-05-03 19:23:43 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import PageTransition from './Components/PageTransition';
 import Lenis from 'lenis';
+import Script from "next/script";
 // import { PreLoaderProvider } from "./Components/PreLoaderContext";
 
 const geistSans = Geist({
@@ -72,6 +73,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>{getPageTitle()}</title>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-GJPRLT2XWC"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics-aw" strategy="afterInteractive">
+            {`
+               window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-GJPRLT2XWC');
+            `}
+          </Script>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="UaRMkghVNJyQGe04TbxWq2ZwHz8YwusRB-A7uaik9k8" />
       </head>
