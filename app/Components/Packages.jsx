4670e6f7f8168cbb80117da4fc8005c228439508
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-07-16 21:47:20 +0500

    gfhf

--- app/Components/Packages.jsx
+++ app/Components/Packages.jsx
@@ -597,6 +597,27 @@ const Packages = () => {
         ))}
       </div>
       )}
+      {region == "Australia" && (
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+        {packageDetailsAus[tab]?.map((pkg, index) => (
+          <div key={index} className="p-8 z-50 bg-gray-100 text-black max-h-[38rem] overflow-y-scroll rounded-2xl shadow-lg">
+            <h2 className="text-xl font-light">{pkg.title}</h2>
+            <div className="flex flex-col justify-center items-center">
+              <p className="font-bold mt-5 text-xs">Starting from</p>
+              <p className="text-3xl text-center  text-yellow-400 font-extrabold">{pkg.price}</p>
+              <p className="text-xs text-center mt-4 text-black">Suitable for potential super-startups and brand revamps for companies.</p>
+              <Link href="/contact"><button className="text-black mt-6 text-center bg-cards px-6 py-4 rounded-md">Let's Start</button></Link>
+              <p className="text-sm pt-5 text-black">{pkg.description}</p>
+              <div className="mt-4 max-h-40 overflow-y-auto text-sm space-y-3 text-left text-black list-disc ">
+                {pkg.features.map((feature, i) => (
+                  <p key={i}>{feature}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      )}
     </section>
   );
 };
