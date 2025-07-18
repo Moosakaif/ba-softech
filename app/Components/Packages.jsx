bfa39c60a7fa57b9cf7e02d1b41c3239a2be3f6a
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-07-18 20:54:30 +0500

    sca

--- app/Components/Packages.jsx
+++ app/Components/Packages.jsx
@@ -1016,6 +1016,46 @@ const Packages = () => {
         features: ["Hat Design", "Invoice Design", "Email Signature Design","2 Stationery Design Set","Packaging Design","UNLIMITED Revisions","T-Shirt Design","T-Shirt Design"],
       },
     ],
+    "Content Writing": [
+      {
+        title: "WEB CONTENT PACKAGE",
+        price: "$200",
+        description: "SEO-optimized blog content.",
+        features: ["Some Salient Features of Our Website Copywriting Services are:", "Creative, Well-Written and 100% Original Content", "According To Your Exact Requirements","FREE Meta details – With each custom page, we will provide a catchy title, keywords and page description.","SEO friendly - Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","Quick Turnaround Time – Within 3 business days!!","Unlimited Revisions - 100% Satisfaction Guaranteed!","Proofing by our in-house experts – 0% mistakes guarantee!","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "ARTICLE WRITING PACKAGE",
+        price: "$1499",
+        description: "Professional website content.",
+        features: ["1 Creative, Fresh & Well-Written Article", "By 1 Professional Copywriter", "Industry Specified Expert Copywriter","400 Words Per Page","3 to 5 Business Days Rotation","5 Revisions","According To Your Exact Requirements","Written on Your Specified Topic/Keyword","Proofing by our in-house experts","Free Submission on any article marketing directories /blog (If required)","SEO friendly – Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","100% Ownership Rights","100% Original Content","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "BLOG WRITING PACKAGE",
+        price: "$2000",
+        description: "Extensive content creation.",
+        features: ["1 Creative, Fresh & Well-Written Article", "By 1 Professional Copywriter", "Industry Specified Expert Copywriter","400 Words Per Page","3 to 5 Business Days Rotation","5 Revisions","According To Your Exact Requirements","Written on Your Specified Topic/Keyword","Proofing by our in-house experts","Free Submission on any article marketing directories /blog (If required)","SEO friendly – Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","100% Ownership Rights","100% Satisfaction Guarantee", "Editing", "Publishing", "Designing" ],
+      },
+    ],
+    "Social Media": [
+      {
+        title: "STARTUP FIRE PACKAGE",
+        price: "$320",
+        description: "For improving engagement.",
+        features: ["3 postings per week (per network) Facebook + Twitter + Instagram + Google+", "Content Creation", "Business Page Optimization","Social Media Strategy (Overview)","Facebook Likes Campaign","Monthly Progress report"],
+      },
+      {
+        title: "EXELLANCE DREAM PACKAGE",
+        price: "$600",
+        description: "Complete social media management.",
+        features: ["Copywriting & Visual designs", "Business Page Optimization", "Ad Campaign Management","Spam monitoring","Monthly Progress report","5 postings per week Facebook + Twitter + Instagram + Google+","Reputation Management","Social Account Setup","Content Creation","Social Media Listening","Query and comments reply"],
+      },
+      {
+        title: "ALL IN ONE SPARK PACKAGE",
+        price: "$1000",
+        description: "Ideal for small businesses.",
+        features: ["Initial Brand Analysis", "Consumer Persona Analysis", "Competitor Analysis - Local and Nationwide","Customized Social Media Strategy","Copywriting - Team of 04 Dedicated Social Media Writers","Advertising - Team of 04 Dedicated Social Media Ads Experts","Content Plan Creation","35 Custom Designed Social Media Posts per month","Monthly Google Ads Management - Google Shopping + Google Search Campaigns","Goal Focused Facebook Ad Campaigns","Community Management - All Community Based Social Networks","Page Management (Facebook, Instagram, Linked In, and Twitter)","Page Spam Management - all social media platforms","Engagement Tracking Setup","Facebook Shop Creation","Detailed Page Moderation - All Social Networks","Strategic Ad Campaign Optimization & A/B Testing","Social Media Listening","Monthly Analytics Reporting - KPI Comparison and more","Social Media Listening"],
+      },
+    ],  
   };
 
   return (
