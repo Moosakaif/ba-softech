dedfc186c84e198b818873afa54361ce61a36912
MoosaKaif <164340042+Moosakaif@users.noreply.github.com>
2025-08-11 22:29:17 +0500

    sdc

--- app/Components/Packages.jsx
+++ app/Components/Packages.jsx
@@ -705,7 +705,176 @@ const Packages = () => {
       },
     ],
   };
-
+  const packageDetailsUAE = {
+    "Logo Design": [
+      {
+        title: "BASIC LOGO PACKAGE",
+        price: "£21",
+        description: "Ideal for startups and brand revamps.",
+        features: ["3 Custom Logo Concepts", "1 Designer", "4 Revisions", "24-48 hours TAT", "100% Unique Design Guarantee"],
+      },
+      {
+        title: "START UP LOGO PACKAGE",
+        price: "£35",
+        description: "Perfect for growing businesses.",
+        features: ["5 Custom Logo Concepts", "2 Designers", "Unlimited Revisions", "48-72 hours TAT", "Final Files in AI, PSD, PNG, PDF"],
+      },
+      {
+        title: "LOGO GROWUP PACKAGE",
+        price: "£45",
+        description: "For established businesses.",
+        features: ["8 Custom Logo Concepts", "3 Designers", "Unlimited Revisions", "Stationary Design", "Free MS Word Letterhead"],
+      },
+      {
+        title: "BA SPECIAL PACKAGE",
+        price: "£90",
+        description: "Comprehensive branding solution.",
+        features: ["UNLIMITED Logo Design Concepts", "By 5 Award Winning Designers", "Stationary Design (Business Card, Letterhead, Envelope)","Icon Design","UNLIMITED Revisions","FREE MS Word Letterhead","Free Email Signature","All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)","100% Satisfaction Guarantee","100% Unique Design Guarantee","48 to 72 hours TAT"],
+      },
+      {
+        title: "BUSINESS LOGO PACKAGE",
+        price: "£125",
+        description: "Comprehensive branding solution.",
+        features: ["Unlimited Logo Concepts", "8 Award-Winning Designers","UNLIMITED Revisions", " 2 Stationary Design Sets (Business Card, Letterhead, Envelope)","Icon Design","Double Sided Flyer Design / Bi-Fold Brochure","FREE MS Word Letterhead","Free Email Signature","All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)","100% Ownership Rights","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+      {
+        title: "GOLD LOGO PACKAGE",
+        price: "£220",
+        description: "Comprehensive branding solution.",
+        features: ["Unlimited Logo Concepts", "8 Award-Winning Designers","Icon Design","UNLIMITED Revisions", "2 Stationary Design Sets (Business Card, Letterhead, Envelope)","FREE MS Word Letterhead","Free Email Signature","3 Page Custom Website","2 Stock Photos","2 Banner Designs","jQuery Slider","All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)","100% Ownership Rights","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+      {
+        title: "ILLUSTRATION PACKAGE",
+        price: "£175",
+        description: "Comprehensive branding solution.",
+        features: ["5 unique Illustrations", "Unlimited color Variations", "Unlimited color Variations","Unlimited Revisions","48 to 72 hours TAT","All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)","100% Ownership Rights","100% Satisfaction Guarantee" , "100% Unique Design Guarantee"],
+      },
+      {
+        title: "MASCOT PACKAGE",
+        price: "£290",
+        description: "Comprehensive branding solution.",
+        features: ["3 Unique mascot Design", "2-3 Combinations", "100% Custom Illustration","Unlimited Revisions","48 to 72 hours TAT","All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)","100% Ownership Rights","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+    ],
+    "Website Development": [
+      {
+        title: "BASIC WEBSITE PACKAGE",
+        price: "£105",
+        description: "Ideal for small businesses and startups.",
+        features: ["2 Stock Images", "3 Pages Website", "1 jQuery Slider Banner", "Contact/Query Form", "Complete W3C Certified HTML","48 to 72 hours TAT","Complete Deployment","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+      {
+        title: "START UP WEBSITE PACKAGE",
+        price: "£195",
+        description: "Suitable for businesses looking for expansion.",
+        features: ["5 Stock Photos", "5 Pages Website", "3 Banner Design", "1 jQuery Slider Banner", "FREE Google Friendly Sitemap","Complete W3C Certified HTML","48 to 72 hours TAT","100% Satisfaction Guarantee","100% Unique Design Guarantee" , "Mobile Responsive will be Additional $200*","CMS will be Additional $250*"],
+      },
+      {
+        title: "PROFESSIONAL WEBSITE PACKAGE",
+        price: "£415",
+        description: "Best for online stores.",
+        features: ["10 Unique Pages Website", "CMS / Admin Panel Support", "8 Stock images", "5 Banner Designs", "1 jQuery Slider Banner","FREE Google Friendly Sitemap","Complete W3C Certified HTML","48 to 72 hours TAT","Complete Deployment","100% Satisfaction Guarantee", "100% Unique Design Guarantee","Mobile Responsive will be Additional $200*"],
+      },
+      {
+        title: "ELITE WEBSITE PACKAGE",
+        price: "£685",
+        description: "Complete web solution.",
+        features: ["Upto 15 Unique Pages Website", "Conceptual and Dynamic Website", "Mobile Responsive", "Online Reservation/Appointment Tool (Optional)", "Online Payment Integration (Optional)","Custom Forms","Lead Capturing Forms (Optional)","Striking Hover Effects","Newsletter Subscription (Optional)","Newsfeed Integration","Social Media Integration","Search Engine Submission","5 Stock Photos","3 Unique Banner Design","1 jQuery Slider Banner","Complete W3C Certified HTML","48 to 72 hours TAT","Complete Deployment","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+      {
+        title: "BUSINESS WEBSITE PACKAGE",
+        price: "£1100",
+        description: "Complete web solution.",
+        features: ["30 Seconds 2D Explainer Video", "Voice - Over & Sound Effects", "Professional Script Writing", "Storyboard", "SEO Meta Tags","Custom Made, Interactive, Dynamic & High End Design","Custom WP (or) Custom PHP Development","1 jQuery Slider Banner","Up to 10 Custom Made Banner Designs","10 Stock Images","Unlimited Revisions","Special Hoover Effects","Content Management System (CMS)","Online Appointment","Online Payment Integration (Optional)","Multi Lingual (Optional)","Custom Dynamic Forms (Optional)","Signup Area (For Newsletters, Offers etc.)","Search Bar","Live Feeds of Social Networks Integration (Optional)","Mobile Responsive","FREE 5 Years Domain Name","Free Google Friendly Sitemap","Search Engine Submission","Complete W3C Certified HTML","Industry Specified Team of Expert Designers and Developers","Complete Deployment","Dedicated Accounts Manager","100% Ownership Rights","100% Satisfaction Guarantee","100% Unique Design Guarantee"],
+      },
+    ],
+    "Animations": [
+      {
+        title: "2D STANDARD LOGO ANIMATION",
+        price: "£78",
+        description: "Simple logo animations.",
+        features: ["3 custom and Unique design", "2 Unique Animation styles", "Camera Angles Positioning", "Lighting/Composing","3 Revisions","Animation and Effects","Dynamic colours","background music","Source files format Mp4 MOV ETC","100% SATISFACTION GURANTEE","100% unique design gurantee"],
+      },
+      {
+        title: "2D ADVANCE LOGO PACKAGE",
+        price: "£150",
+        description: "Short explainer video for businesses.",
+        features: ["3 custom and Unique design", "3 Unique Animation styles", "Camera Angles Positioning", "Lighting/Composing","unlimited Revisions","Animation and Effects","Dynamic colours","background music","Source files format Mp4 MOV ETC","100% SATISFACTION GURANTEE","100% unique design gurantee"],
+      },
+      {
+        title: "3D STANDARD LOGO ANIMATION",
+        price: "£200",
+        description: "Ideal for corporate storytelling.",
+        features: ["5 custom and Unique design", "5 Unique Animation styles", "Camera Angles Positioning", "Lighting/Composing","unlimited Revisions","Animation and VFX","Dynamic colours","background music","Source files format Mp4 MOV ETC","100% SATISFACTION GURANTEE","100% unique design gurantee"],
+      },
+      {
+        title: "3D ADVANCE ANIMATION",
+        price: "£250",
+        description: "High-end animation package.",
+        features: ["UNLIMITED custom and Unique design", "UNLIMITED Unique Animation styles", "Camera Angles Positioning", "Lighting/Composing","unlimited Revisions","Animation and VFX","Music and foley","All source files format Included Mp4 MOV ETC","100% SATISFACTION GURANTEE","100% unique design gurantee"],
+      },
+    ],
+    "Branding": [
+      {
+        title: "STARTUP COLLATERAL PACKAGE",
+        price: "£55",
+        description: "Basic branding essentials.",
+        features: ["2 Stationery Design Set", "FREE Fax Template", "Print Ready Formats","UNLIMITED Revisions","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "COLLATERAL CLASSIC PACKAGE",
+        price: "£105",
+        description: "Full branding package for businesses.",
+        features: ["2 Stationery Design Set", "UNLIMITED Revisions", "Flyer Design","Brochure Design (Bi-fold/Tri-fold)","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "PREMIUM COLLATERAL PACKAGE",
+        price: "£200",
+        description: "Complete identity package.",
+        features: ["Hat Design", "Invoice Design", "Email Signature Design","2 Stationery Design Set","Packaging Design","UNLIMITED Revisions","T-Shirt Design","T-Shirt Design"],
+      },
+    ],
+    "Content Writing": [
+      {
+        title: "WEB CONTENT PACKAGE",
+        price: "£125",
+        description: "SEO-optimized blog content.",
+        features: ["Some Salient Features of Our Website Copywriting Services are:", "Creative, Well-Written and 100% Original Content", "According To Your Exact Requirements","FREE Meta details – With each custom page, we will provide a catchy title, keywords and page description.","SEO friendly - Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","Quick Turnaround Time – Within 3 business days!!","Unlimited Revisions - 100% Satisfaction Guaranteed!","Proofing by our in-house experts – 0% mistakes guarantee!","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "ARTICLE WRITING PACKAGE",
+        price: "£770",
+        description: "Professional website content.",
+        features: ["1 Creative, Fresh & Well-Written Article", "By 1 Professional Copywriter", "Industry Specified Expert Copywriter","400 Words Per Page","3 to 5 Business Days Rotation","5 Revisions","According To Your Exact Requirements","Written on Your Specified Topic/Keyword","Proofing by our in-house experts","Free Submission on any article marketing directories /blog (If required)","SEO friendly – Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","100% Ownership Rights","100% Original Content","100% Satisfaction Guarantee"],
+      },
+      {
+        title: "BLOG WRITING PACKAGE",
+        price: "£1020",
+        description: "Extensive content creation.",
+        features: ["1 Creative, Fresh & Well-Written Article", "By 1 Professional Copywriter", "Industry Specified Expert Copywriter","400 Words Per Page","3 to 5 Business Days Rotation","5 Revisions","According To Your Exact Requirements","Written on Your Specified Topic/Keyword","Proofing by our in-house experts","Free Submission on any article marketing directories /blog (If required)","SEO friendly – Your keyword(s) will be placed in the title, the first & last paragraphs and throughout the web copy in a natural and fluent manner","100% Ownership Rights","100% Satisfaction Guarantee", "Editing", "Publishing", "Designing" ],
+      },
+    ],
+    "Social Media": [
+      {
+        title: "STARTUP FIRE PACKAGE",
+        price: "£199",
+        description: "For improving engagement.",
+        features: ["3 postings per week (per network) Facebook + Twitter + Instagram + Google+", "Content Creation", "Business Page Optimization","Social Media Strategy (Overview)","Facebook Likes Campaign","Monthly Progress report"],
+      },
+      {
+        title: "EXELLANCE DREAM PACKAGE",
+        price: "£340",
+        description: "Complete social media management.",
+        features: ["Copywriting & Visual designs", "Business Page Optimization", "Ad Campaign Management","Spam monitoring","Monthly Progress report","5 postings per week Facebook + Twitter + Instagram + Google+","Reputation Management","Social Account Setup","Content Creation","Social Media Listening","Query and comments reply"],
+      },
+      {
+        title: "ALL IN ONE SPARK PACKAGE",
+        price: "£555",
+        description: "Ideal for small businesses.",
+        features: ["Initial Brand Analysis", "Consumer Persona Analysis", "Competitor Analysis - Local and Nationwide","Customized Social Media Strategy","Copywriting - Team of 04 Dedicated Social Media Writers","Advertising - Team of 04 Dedicated Social Media Ads Experts","Content Plan Creation","35 Custom Designed Social Media Posts per month","Monthly Google Ads Management - Google Shopping + Google Search Campaigns","Goal Focused Facebook Ad Campaigns","Community Management - All Community Based Social Networks","Page Management (Facebook, Instagram, Linked In, and Twitter)","Page Spam Management - all social media platforms","Engagement Tracking Setup","Facebook Shop Creation","Detailed Page Moderation - All Social Networks","Strategic Ad Campaign Optimization & A/B Testing","Social Media Listening","Monthly Analytics Reporting - KPI Comparison and more","Social Media Listening"],
+      },
+    ],
+  };
   return (
     <section className={`${font.className} flex flex-col text-white justify-center items-center p-12 bg-black z-50`}>
       <h1 className="text-2xl text-center md:text-5xl text-cards font-bold">See Our Packages</h1>
@@ -815,7 +984,7 @@ const Packages = () => {
       )}
       {region == "UAE" && (
       <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-        {packageDetailsPkr[tab]?.map((pkg, index) => (
+        {packageDetailsUAE[tab]?.map((pkg, index) => (
           <div key={index} className="p-8 z-50 bg-gray-100 text-black max-h-[38rem] overflow-y-scroll rounded-2xl shadow-lg">
             <h2 className="text-xl font-light">{pkg.title}</h2>
             <div className="flex flex-col justify-center items-center">
